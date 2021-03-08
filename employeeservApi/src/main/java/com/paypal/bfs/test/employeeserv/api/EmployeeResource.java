ed02e793a41516ddf85268147bc9848c6546fa26
subramanyajt <subramanyajt@gmail.com>
2021-03-08 23:43:29 +0530

    Add files via upload

---
+++ employeeservApi/src/main/java/com/paypal/bfs/test/employeeserv/api/EmployeeResource.java
@@ -0,0 +1,36 @@
+package com.paypal.bfs.test.employeeserv.api;
+
+import com.paypal.bfs.test.employeeserv.api.model.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
+/**
+ * Interface for employee resource operations.
+ */
+public interface EmployeeResource {
+
+    /**
+     * Retrieves the {@link Employee} resource by id.
+     *
+     * @param id employee id.
+     * @return {@link Employee} resource.
+     */
+    @RequestMapping("/v1/bfs/employees/{id}")
+    ResponseEntity<Employee> employeeGetById(@PathVariable("id") String id);
+
+    // ----------------------------------------------------------
+    // TODO - add a new operation for creating employee resource.
+    // ----------------------------------------------------------
+
+    @PostMapping(value = "/v1/bfs/employee", consumes = APPLICATION_JSON_UTF8_VALUE)
+    ResponseEntity createEmployee(@Valid @RequestBody final Employee employee);
+
+}
---
+++ employeeservApi/target/generated-sources/jsonschema2pojo/com/paypal/bfs/test/employeeserv/api/model/Employee.java
@@ -0,0 +1,379 @@
+
+package com.paypal.bfs.test.employeeserv.api.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Employee resource
+ * <p>
+ * Employee resource object
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id",
+    "first_name",
+    "last_name",
+    "date_of_birth",
+    "address_line1",
+    "address_line2",
+    "city",
+    "state",
+    "country",
+    "zip_code"
+})
+public class Employee {
+
+    /**
+     * employee id
+     * 
+     */
+    @JsonProperty("id")
+    @JsonPropertyDescription("employee id")
+    private Integer id;
+    /**
+     * first name
+     * (Required)
+     * 
+     */
+    @JsonProperty("first_name")
+    @JsonPropertyDescription("first name")
+    private String firstName;
+    /**
+     * last name
+     * (Required)
+     * 
+     */
+    @JsonProperty("last_name")
+    @JsonPropertyDescription("last name")
+    private String lastName;
+    /**
+     * date of birth
+     * (Required)
+     * 
+     */
+    @JsonProperty("date_of_birth")
+    @JsonPropertyDescription("date of birth")
+    private String dateOfBirth;
+    /**
+     * address line 1
+     * (Required)
+     * 
+     */
+    @JsonProperty("address_line1")
+    @JsonPropertyDescription("address line 1")
+    private String addressLine1;
+    /**
+     * address line 2
+     * 
+     */
+    @JsonProperty("address_line2")
+    @JsonPropertyDescription("address line 2")
+    private String addressLine2;
+    /**
+     * city
+     * (Required)
+     * 
+     */
+    @JsonProperty("city")
+    @JsonPropertyDescription("city")
+    private String city;
+    /**
+     * state
+     * (Required)
+     * 
+     */
+    @JsonProperty("state")
+    @JsonPropertyDescription("state")
+    private String state;
+    /**
+     * country
+     * (Required)
+     * 
+     */
+    @JsonProperty("country")
+    @JsonPropertyDescription("country")
+    private String country;
+    /**
+     * zip code
+     * (Required)
+     * 
+     */
+    @JsonProperty("zip_code")
+    @JsonPropertyDescription("zip code")
+    private String zipCode;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * employee id
+     * 
+     */
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * employee id
+     * 
+     */
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * first name
+     * (Required)
+     * 
+     */
+    @JsonProperty("first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * first name
+     * (Required)
+     * 
+     */
+    @JsonProperty("first_name")
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * last name
+     * (Required)
+     * 
+     */
+    @JsonProperty("last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * last name
+     * (Required)
+     * 
+     */
+    @JsonProperty("last_name")
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * date of birth
+     * (Required)
+     * 
+     */
+    @JsonProperty("date_of_birth")
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * date of birth
+     * (Required)
+     * 
+     */
+    @JsonProperty("date_of_birth")
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * address line 1
+     * (Required)
+     * 
+     */
+    @JsonProperty("address_line1")
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    /**
+     * address line 1
+     * (Required)
+     * 
+     */
+    @JsonProperty("address_line1")
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    /**
+     * address line 2
+     * 
+     */
+    @JsonProperty("address_line2")
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    /**
+     * address line 2
+     * 
+     */
+    @JsonProperty("address_line2")
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    /**
+     * city
+     * (Required)
+     * 
+     */
+    @JsonProperty("city")
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * city
+     * (Required)
+     * 
+     */
+    @JsonProperty("city")
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * state
+     * (Required)
+     * 
+     */
+    @JsonProperty("state")
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * state
+     * (Required)
+     * 
+     */
+    @JsonProperty("state")
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    /**
+     * country
+     * (Required)
+     * 
+     */
+    @JsonProperty("country")
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * country
+     * (Required)
+     * 
+     */
+    @JsonProperty("country")
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * zip code
+     * (Required)
+     * 
+     */
+    @JsonProperty("zip_code")
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    /**
+     * zip code
+     * (Required)
+     * 
+     */
+    @JsonProperty("zip_code")
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Employee.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("id");
+        sb.append('=');
+        sb.append(((this.id == null)?"<null>":this.id));
+        sb.append(',');
+        sb.append("firstName");
+        sb.append('=');
+        sb.append(((this.firstName == null)?"<null>":this.firstName));
+        sb.append(',');
+        sb.append("lastName");
+        sb.append('=');
+        sb.append(((this.lastName == null)?"<null>":this.lastName));
+        sb.append(',');
+        sb.append("dateOfBirth");
+        sb.append('=');
+        sb.append(((this.dateOfBirth == null)?"<null>":this.dateOfBirth));
+        sb.append(',');
+        sb.append("addressLine1");
+        sb.append('=');
+        sb.append(((this.addressLine1 == null)?"<null>":this.addressLine1));
+        sb.append(',');
+        sb.append("addressLine2");
+        sb.append('=');
+        sb.append(((this.addressLine2 == null)?"<null>":this.addressLine2));
+        sb.append(',');
+        sb.append("city");
+        sb.append('=');
+        sb.append(((this.city == null)?"<null>":this.city));
+        sb.append(',');
+        sb.append("state");
+        sb.append('=');
+        sb.append(((this.state == null)?"<null>":this.state));
+        sb.append(',');
+        sb.append("country");
+        sb.append('=');
+        sb.append(((this.country == null)?"<null>":this.country));
+        sb.append(',');
+        sb.append("zipCode");
+        sb.append('=');
+        sb.append(((this.zipCode == null)?"<null>":this.zipCode));
+        sb.append(',');
+        sb.append("additionalProperties");
+        sb.append('=');
+        sb.append(((this.additionalProperties == null)?"<null>":this.additionalProperties));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
